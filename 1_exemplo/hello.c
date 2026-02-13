5e8e0894f2b33aa68b8728efc9ac3167f741ecf1
Luan Lima <luanemerson8@gmail.com>
2026-02-13 12:20:56 -0300

    Study

---
+++ 1_exemplo/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Inclui a biblioteca de entrada e saída padrão
+
+int main() {
+    // printf() exibe o texto na tela
+    printf("Hello, World!\n"); 
+    
+    return 0; // Indica que o programa foi finalizado com sucesso
+}
